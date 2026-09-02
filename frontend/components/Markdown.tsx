f9cf239fecebdce3cfbde1b2a68b54d9d7363924
Si-Nan-Si-Mu <frdshkg-sys@gmail.com>
2026-09-02 15:21:28 +0800

    feat: proper markdown display location

--- frontend/components/Markdown.tsx
+++ frontend/components/Markdown.tsx
@@ -4,5 +4,16 @@ import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 
 export default function Markdown({ content }: { content: string }) {
-  return <ReactMarkdown remarkPlugins={[remarkGfm]}>{content}</ReactMarkdown>;
+  return (
+    <ReactMarkdown
+      remarkPlugins={[remarkGfm]}
+      components={{
+        a: ({ node, ...props }) => (
+          <a {...props} target="_blank" rel="noopener noreferrer" />
+        ),
+      }}
+    >
+      {content}
+    </ReactMarkdown>
+  );
 }
